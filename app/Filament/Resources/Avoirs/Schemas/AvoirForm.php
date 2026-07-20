7b65624bf25ab0201eff13471dfa1cd6d25d0ecd
Nexus ERP <contact@nexus-erp.dz>
2026-07-21 00:49:46 +0100

    fix(Filament 4): Section import -> Filament\Schemas\Components\Section

--- app/Filament/Resources/Avoirs/Schemas/AvoirForm.php
+++ app/Filament/Resources/Avoirs/Schemas/AvoirForm.php
@@ -4,7 +4,7 @@ namespace App\Filament\Resources\Avoirs\Schemas;
 
 use App\Enums\StatutAvoir;
 use Filament\Forms\Components\DatePicker;
-use Filament\Forms\Components\Section;
+use Filament\Schemas\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
